1f2ecaef10bc4fde6b425750fbf6fa85eaefec01
nakitamccool <nakitamccool@gmail.com>
2017-03-28 16:43:08 +0100

    created about page (need to handle emails). Navbar links/ urls activated

--- hirecycle/urls.py
+++ hirecycle/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from adverts import urls as adverts_urls
 from accounts import urls as accounts_urls
-from home.views import index
+from home.views import index, about
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
@@ -25,7 +25,8 @@ from django.views.static import serve
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
-    url(r'^index/$', index, name='index'),
+    url(r'^index/', index, name='index'),
+    url(r'^about/', about, name='about'),
     url(r'^ads/', include(adverts_urls)),
     url(r'^account/', include(accounts_urls)),
 ]

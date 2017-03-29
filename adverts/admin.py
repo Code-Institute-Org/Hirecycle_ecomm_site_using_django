0ad47f60658b2e32ea399f2c895ce7d7f8376051
nakitamccool <nakitamccool@gmail.com>
2017-03-29 18:18:25 +0100

    Created new model for insurance cover and loaded to db

--- adverts/admin.py
+++ adverts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Advert
+from .models import Advert, InsurancePackage
 
 # Register your models here.
 admin.site.register(Advert)
+
+admin.site.register(InsurancePackage)
--- adverts/models.py
+++ adverts/models.py
@@ -17,29 +17,6 @@ class Advert(models.Model):
     pickup_location = models.CharField(max_length=200)
     original_retail_price = models.DecimalField(max_digits=10, decimal_places=2)
     daily_rental_rate = models.DecimalField(max_digits=10, decimal_places=2)
-    daily_insurance_cover_rate = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    no_longer_needed = models.CharField(max_length=100, default=1.00)
-    # insurance_package_choices = (
-    #     (1.0,"I don't need insurance"),
-    #     (1.2,"Bronze"),
-    #     (1.4,"Silver"),
-    #     (1.5,"Gold"),
-    # )
-    insurance_package = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        choices=(
-        (1.0, "I don't need insurance"),
-        (1.2, "Bronze"),
-        (1.4, "Silver"),
-        (1.5, "Gold"))
-    )
-
-    @property
-    def advertised_daily_rental_rate(self):
-        daily_rental_rate = self.daily_rental_rate
-        insurance_package = self.insurance_package
-        return (daily_rental_rate * insurance_package)
 
     def publish(self):
         self.created_date = timezone.now()
@@ -47,3 +24,47 @@ class Advert(models.Model):
 
     def __unicode__(self):
         return self.item
+
+class InsurancePackage(models.Model):
+
+    NoInsurance = "I don't need insurance"
+    Bronze = "Bronze"
+    Silver = "Silver"
+    Gold = "Gold"
+
+    insurance_package_choices=(
+        (NoInsurance, "I don't need insurance"),
+        (Bronze, "Bronze"),
+        (Silver, "Silver"),
+        (Gold, "Gold")
+    )
+
+    insurance_package = models.CharField(max_length=100, choices=insurance_package_choices, default=NoInsurance)
+    insurance_package_rate = models.DecimalField(max_digits=4, decimal_places=2, default=1)
+    def publish(self):
+        self.save()
+
+    def __unicode__(self):
+        return self.insurance_package
+
+        # @property
+        # def advertised_daily_rental_rate(self):
+        #     daily_rental_rate = self.daily_rental_rate
+        #     insurance_package = self.insurance_package
+        #     return (daily_rental_rate * insurance_package)
+    # daily_insurance_cover_rate = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    # # insurance_package_choices = (
+    # #     (1.0,"I don't need insurance"),
+    # #     (1.2,"Bronze"),
+    # #     (1.4,"Silver"),
+    # #     (1.5,"Gold"),
+    # # )
+    # insurance_package = models.DecimalField(
+    #     max_digits=10,
+    #     decimal_places=2,
+    #     choices=(
+    #     (1.0, "I don't need insurance"),
+    #     (1.2, "Bronze"),
+    #     (1.4, "Silver"),
+    #     (1.5, "Gold"))
+    # )

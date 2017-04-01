9486d60f4484ce2bc21fc5e12fe8d2a708cf1ce8
nakitamccool <nakitamccool@gmail.com>
2017-04-01 10:34:30 +0100

    removed ins package from form as need to rework model

--- adverts/forms.py
+++ adverts/forms.py
@@ -14,5 +14,4 @@ class AdvertPostForm(forms.ModelForm):
             'image',
             'original_retail_price',
             'daily_rental_rate',
-            'insurance_package',
             'pickup_location')
